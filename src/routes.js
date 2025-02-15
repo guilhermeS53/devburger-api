9de9489ed907cefaae1dbc58d5f2d249072c46b0
Guilherme Sousa <guilherme.gs17@outlook.com>
2025-02-15 11:43:50 -0300

    adicao do pacote npm uuid v4

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+import { v4 } from 'uuid';
+import User from './app/models/User';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-    res.status(200).json({ message: 'Hello World' });
+router.get('/', async (req, res) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'Guilherme',
+        email: 'guilhermetxt53@gmail.com',
+        password_hash: 'hbfhbfsdjfjsdnfj',
+    });
+    return res.status(201).json(user);
 });
 
 export default router;
