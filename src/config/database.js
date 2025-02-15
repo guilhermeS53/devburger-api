779e6a1f919dc339a329d58acdda21d503b6dc25
Guilherme Sousa <guilherme.gs17@outlook.com>
2025-02-15 11:28:13 -0300

    configuracao do banco de dados

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5434,
+  username: 'postgres',
+  password: 'postgre',
+  database: 'devburger',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
