18493c3e57dc3aca083c9d0664d5e0c24e54b268
Guilherme Sousa <guilherme.gs17@outlook.com>
2025-02-16 17:42:16 -0300

    feat: adicao de validacao de dados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,8 +1,22 @@
 import { v4 } from 'uuid';
 import User from '../models/User';
+import * as Yup from 'yup';
 
 class UserController {
     async store(req, res) {
+            const schema = Yup.object().shape({
+            name: Yup.string().required(),
+            email: Yup.string().email().required(),
+            password_hash: Yup.string().required().min(6),
+            admin: Yup.boolean(),
+        });
+
+        try {
+           schema.validateSync(req.body, { abortEarly: false });
+        } catch (err) {
+            return res.status(400).json({ error: err.errors });
+        }
+
         const { name, email, password_hash, admin } = req.body;
 
         const user = await User.create({
