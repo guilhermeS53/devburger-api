3031c1be25aaab6b635fa5ecfbc0d73f7e14e820
Guilherme Sousa <guilherme.gs17@outlook.com>
2025-02-18 22:36:23 -0300

    feat: add password verification method to User model

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,29 +1,33 @@
-import { Model, DataTypes } from 'sequelize';
-import bcrypt from 'bcryptjs';
+import { Model, DataTypes } from "sequelize";
+import bcrypt from "bcryptjs";
 
 class User extends Model {
-    static init(sequelize) {
-        super.init(
-            {
-                name: DataTypes.STRING,
-                email: DataTypes.STRING,
-                password: DataTypes.VIRTUAL,
-                password_hash: DataTypes.STRING,
-                admin: DataTypes.BOOLEAN,
-            },
-            {
-                sequelize,
-                modelName: 'User',
-            }
-        );
-        this.addHook('beforeSave', async (user) => {
-            if (user.password) {
-                user.password_hash = await bcrypt.hash(user.password, 8);
-            }
-        });
+  static init(sequelize) {
+    super.init(
+      {
+        name: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.VIRTUAL,
+        password_hash: DataTypes.STRING,
+        admin: DataTypes.BOOLEAN,
+      },
+      {
+        sequelize,
+        modelName: "User",
+      }
+    );
+    this.addHook("beforeSave", async (user) => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
 
-        return this;
-    }
+    return this;
+  }
+
+  async checkPassword(password) {
+    return bcrypt.compare(password, this.password_hash);
+  }
 }
 
 export default User;
