0c89fc79e2b14c70e807e9a15b59bad8c2f46bcb
Guilherme Sousa <guilherme.gs17@outlook.com>
2025-03-17 21:53:23 -0300

    feat: add authentication middleware to protect product routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from "./config/multer";
 import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import ProductController from "./app/controllers/ProductController";
+import authMiddleware from "./app/middlewares/auth";
 
 const router = Router();
 
@@ -11,6 +12,8 @@ const upload = multer(multerConfig);
 
 router.post("/users", UserController.store);
 router.post("/session", SessionController.store);
+
+router.use(authMiddleware);
 router.post("/products", upload.single("file"), ProductController.store);
 router.get("/products", ProductController.index);
 
